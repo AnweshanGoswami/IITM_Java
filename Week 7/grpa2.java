a87d9c5a479e16955027993d3dcdf73bc509bd73
Anweshan Goswami <anweshangoswami@gmail.com>
2024-07-16 18:17:00 +0530

    grded programming assignmemts

---
+++ Week 7/grpa2.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+//define user defined exception InvalidInputEx
+//We do not know when this exception will be triggered
+//And we need to do exactly that in the set value function
+//We cant throw this function in the catch block of another error
+class InvalidInputEx extends Exception{
+    //Constructor
+    public InvalidInputEx(String message){
+    super(message);
+    }
+}
+
+//define the class IntList with 
+class IntList{
+    
+    //instance variable of int[]
+    public int [] list = new int[5]; 
+    
+    //Constructor initialises default values to zero
+    public IntList(){
+        for(int i=0;i<5;i++){
+        list[i] = 0;
+        }
+    }
+    
+    //and methods set_value, getArray()
+    
+    /*public void set_value(int index, int value) throws InvalidInputEx{
+        try{
+            list[index] = value;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            throw new InvalidInputEx("Index " + index + " out of bounds for length 5");
+        }*/
+
+    //The problem with what we just did there is that its a try and catch block. Therefore, it will try something, and upon error
+    //the catch block will try to fix that issue. 
+    //However, throwing another exception is not fixing the issue
+    //Therefore, we need to  check if the conditions are being met and if not, then we throw the error
+    //Which travels up to the main finction and is dealt with in their try and catch block
+
+    //and methods set_value, getArray()
+    public void set_value(int index, int value) throws InvalidInputEx{
+        
+        if (index < 0 || index > 4)
+        throw new InvalidInputEx("Index " + index + " out of bounds for length 5");
+
+        list[index] = value;
+        
+
+    }
+    
+    public int[] getArray(){
+        return (list);
+    }
+    
+}
+
+
+
+
+public class grpa2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        IntList ilist = new IntList();
+        try {
+            for(int i = 0; i < 5; i++) {			
+                int n = sc.nextInt();
+                int m = sc.nextInt();
+                ilist.set_value(n, m);
+            }
+        }
+        catch(InvalidInputEx e) {
+            System.out.println(e.getMessage());
+            Throwable ori = e.getCause();
+            System.out.println(ori.getMessage());
+        }	
+        int[] i_arr = ilist.getArray();
+        for(int i = 0; i < i_arr.length; i++)
+            System.out.print(i_arr[i] + " ");
+    }
+}
---
+++ Week 7/scannertest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class scannertest {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your string below");
+        int a = sc.nextInt();
+        System.out.println("The number that you entered was as follows"+a);
+        
+
+    }
+}
